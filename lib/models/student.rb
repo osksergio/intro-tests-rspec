14de11430a96b25acefb4b2715b3fbceb9fe9d2f
Sergio Oseko <osk.sergio@gmail.com>
2025-02-15 20:48:17 -0300

    test: mais testes e modificacão do model

--- lib/models/student.rb
+++ lib/models/student.rb
@@ -1,5 +1,15 @@
 class Student
+  attr_accessor :name
+
+  def initialize(name = 'Unknown')
+    @name = name
+  end
+
   def self.say_hi
     'hi'
+  end
+
+  def say_hi
+    "hi, i'm #{name}"
   end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -6,4 +6,16 @@ describe Student do
       expect(described_class.say_hi).to eq('hi')
     end
   end
+
+  context '#say_hi' do
+    it 'should present her/him whit name' do
+      subject.name = 'Sarah'
+
+      expect(subject.say_hi).to eq('hi, i\'m Sarah')
+    end
+
+    it 'should present her/him with default name' do
+      expect(subject.say_hi).to eq('hi, i\'m Unknown')
+    end
+  end
 end
